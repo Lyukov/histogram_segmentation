4cea374b1de05643250c0058afd4295e2d6c7e5e
Dmitry <d@lyukov.com>
2017-10-27 00:58:09 +0300

    Implemented random access iterator for Nodes, access through array and sort method

--- histogram.hpp
+++ histogram.hpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <cstdint>
 #include <iostream>
 #include <vector>
 
-#define BLOCKSIZE 65536
+#define BITS 16
+#define BLOCKSIZE 1 << BITS
+#define MASK BLOCKSIZE - 1
 
 template <size_t N, typename T>
 class Key {
@@ -55,6 +58,7 @@ struct Node {
     Node *right;
     // List
     Node *next;
+
     Node() {
         count = 0.;
         parent = NULL;
@@ -69,6 +73,7 @@ class Histogram {
     Node<N, T> *free;
     std::vector<Node<N, T> *> body;
     Node<N, T> *head;
+    size_t _size;
 
     Histogram();
     Node<N, T> *allocate();
@@ -80,18 +85,84 @@ class Histogram {
     void remove(const Key<N, T> key);
     void sort();
     void rebuild_tree();
+    size_t size() const { return _size; }
     Node<N, T> &operator[](size_t index);
+};
+
+template <size_t N, typename T>
+class NodeIterator : public std::iterator<std::random_access_iterator_tag, Node<N, T>> {
+   public:
+    NodeIterator(const Node<N, T> **_body, size_t _ind = 0) : body(_body), ind(ind) {}
+    NodeIterator(const Histogram<N, T> &hist, size_t _ind = 0) : body(&(hist[0])), ind(ind) {}
+    NodeIterator(const NodeIterator &rhs) : body(rhs.body), ind(rhs.ind) {}
+    inline NodeIterator &operator+=(int rhs) {
+        ind += rhs;
+        return *this;
+    }
+    inline NodeIterator &operator-=(int rhs) {
+        ind -= rhs;
+        return *this;
+    }
+    inline Node<N, T> &operator*() const { return body[ind >> BITS][ind & MASK]; }
+    inline Node<N, T> *operator->() const { return &(body[ind >> BITS][ind & MASK]); }
+    inline Node<N, T> &operator[](int rhs) const {
+        return body[(ind + rhs) >> BITS][(ind + rhs) & MASK];
+    }
+
+    inline NodeIterator &operator++() {
+        ++ind;
+        return *this;
+    }
+    inline NodeIterator &operator--() {
+        --ind;
+        return *this;
+    }
+    inline NodeIterator operator++(int) {
+        NodeIterator tmp(*this);
+        ++ind;
+        return tmp;
+    }
+    inline NodeIterator operator--(int) {
+        NodeIterator tmp(*this);
+        --ind;
+        return tmp;
+    }
+    inline int operator-(const NodeIterator &rhs) const { return ind - rhs.ind; }
+    inline NodeIterator operator+(int rhs) const { return NodeIterator(body, ind + rhs); }
+    inline NodeIterator operator-(int rhs) const { return NodeIterator(body, ind - rhs); }
+    friend inline NodeIterator operator+(int lhs, const NodeIterator &rhs) {
+        return NodeIterator(rhs.body, lhs + rhs.ind);
+    }
+    friend inline NodeIterator operator-(int lhs, const NodeIterator &rhs) {
+        return NodeIterator(rhs.body, lhs - rhs.ind);
+    }
+
+    inline bool operator==(const NodeIterator &rhs) const {
+        return body == rhs.body && ind == rhs.ind;
+    }
+    inline bool operator!=(const NodeIterator &rhs) const {
+        return body != rhs.body || ind != rhs.ind;
+    }
+    inline bool operator>(const NodeIterator &rhs) const { return ind > rhs.ind; }
+    inline bool operator<(const NodeIterator &rhs) const { return ind < rhs.ind; }
+    inline bool operator>=(const NodeIterator &rhs) const { return ind >= rhs.ind; }
+    inline bool operator<=(const NodeIterator &rhs) const { return ind <= rhs.ind; }
+
+   private:
+    Node<N, T> **body;
+    size_t ind;
 };
 
 template <size_t N, typename T>
 Histogram<N, T>::Histogram() {
     free = allocate();
     head = NULL;
+    _size = 0;
 }
 
 template <size_t N, typename T>
 Node<N, T> *Histogram<N, T>::allocate() {
-    std::vector<Node<N, T> > __p(BLOCKSIZE);
+    std::vector<Node<N, T>> __p(BLOCKSIZE);
     body.push_back(&(__p[0]));
     for(size_t i = 0; i < BLOCKSIZE - 1; ++i) {
         __p[i].next = &(__p[i + 1]);
@@ -108,6 +179,7 @@ Node<N, T> *Histogram<N, T>::get_new() {
     Node<N, T> *result = free;
     free = free.next;
     result->next = NULL;
+    ++_size;
     return result;
 }
 
@@ -149,4 +221,15 @@ Node<N, T> &Histogram<N, T>::as_tree_at(const Key<N, T> key) {
 template <size_t N, typename T>
 void Histogram<N, T>::add(double w, const Key<N, T> key) {
     as_tree_at(key).count += w;
+}
+
+template <size_t N, typename T>
+Node<N, T> &Histogram<N, T>::operator[](size_t index) {
+    return body[index >> BITS][index & MASK];
+}
+
+template <size_t N, typename T>
+void Histogram<N, T>::sort() {
+    std::sort(NodeIterator<N, T>(*this, 0), NodeIterator<N, T>(*this, _size),
+              [](const Node<N, T> &n1, const Node<N, T> &n2) { return n1.count < n2.count; });
 }
